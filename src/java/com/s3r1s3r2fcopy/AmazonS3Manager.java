fa1476b9657135b9d95dd4861fba632ab5142038
Bohdan Pylypchenko <bpylypchenko@griddynamics.com>
2023-06-05 16:55:55 +0300

    Being able to perform multi-regional multi-account copy

---
+++ src/java/com/s3r1s3r2fcopy/AmazonS3Manager.java
@@ -0,0 +1,62 @@
+package com.s3r1s3r2fcopy;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicSessionCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenService;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder;
+import com.amazonaws.services.securitytoken.model.AssumeRoleRequest;
+import com.amazonaws.services.securitytoken.model.AssumeRoleResult;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AmazonS3Manager {
+
+    @Value("${app.amazonS3manager.roleArn}")
+    private String roleArn;
+
+    @Value("${app.amazonS3manager.roleSessionName}")
+    private String roleSessionName;
+
+    private AWSSecurityTokenService stsClient;
+
+    private AmazonS3 amazonS3;
+
+    @PostConstruct
+    private void initializeS3() {
+        // Create the STS client
+        stsClient = AWSSecurityTokenServiceClientBuilder.standard().build();
+
+        // Create the AssumeRoleRequest
+        AssumeRoleRequest assumeRoleRequest = new AssumeRoleRequest().withRoleArn(roleArn)
+                                                                     .withRoleSessionName(roleSessionName);
+
+        // Assume the role
+        AssumeRoleResult assumeRoleResult = stsClient.assumeRole(assumeRoleRequest);
+
+        // Getting assumed credentials
+        BasicSessionCredentials awsCredentials = new BasicSessionCredentials(assumeRoleResult.getCredentials().getAccessKeyId(),
+                                                                             assumeRoleResult.getCredentials().getSecretAccessKey(),
+                                                                             assumeRoleResult.getCredentials().getSessionToken());
+
+        // Initializing amazon s3
+        amazonS3 = AmazonS3ClientBuilder.standard()
+                                        .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                                        .enableForceGlobalBucketAccess()
+                                        .build();
+    }
+
+    public AmazonS3 getAmazonS3() {
+        return amazonS3;
+    }
+
+    @PreDestroy
+    private void cleanupCredentials() {
+        stsClient.shutdown();
+    }
+
+}
--- src/java/com/s3r1s3r2fcopy/S3CopyUtility.java
+++ src/java/com/s3r1s3r2fcopy/S3CopyUtility.java
@@ -1,37 +1,43 @@
 package com.s3r1s3r2fcopy;
 
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CopyObjectRequest;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
-import com.s3r1s3r2fcopy.S3ClientProvider.S3ClientProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
-public class S3CopyUtility {
+@Component
+public class S3CopyUtility implements ApplicationRunner {
 
-    private final String sourceBucketName;
+    @Value("${app.s3bucket.sourceBucketName}")
+    private String sourceBucketName;
 
-    private final String targetBucketName;
+    @Value("${app.s3bucket.targetBucketName}")
+    private String targetBucketName;
 
-    private final S3ClientProvider s3ClientProvider;
+    private final AmazonS3 amazonS3;
 
-    public S3CopyUtility(String sourceBucketName, String targetBucketName,
-                         S3ClientProvider s3ClientProvider) {
-        this.sourceBucketName = sourceBucketName;
-        this.targetBucketName = targetBucketName;
-        this.s3ClientProvider = s3ClientProvider;
+    @Autowired
+    public S3CopyUtility(AmazonS3Manager amazonS3Manager) {
+        this.amazonS3 = amazonS3Manager.getAmazonS3();
     }
 
-    public void run() {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
         // Dummy logging
         System.out.println("Start copy");
 
         // Copying
-        for (S3ObjectSummary objectSummary : s3ClientProvider.getS3Client4ListRequest()
-                                                             .listObjects(sourceBucketName)
-                                                             .getObjectSummaries()) {
+        for (S3ObjectSummary objectSummary : amazonS3.listObjects(sourceBucketName)
+                .getObjectSummaries()) {
             // Creating request for current object
             CopyObjectRequest copyRequest = new CopyObjectRequest(sourceBucketName, objectSummary.getKey(),
                                                                   targetBucketName, objectSummary.getKey());
             // Copying current object
-            s3ClientProvider.getS3Client4CopyRequest().copyObject(copyRequest);
+            amazonS3.copyObject(copyRequest);
 
             // Dummy logging
             System.out.printf("Copied object: %s\n", objectSummary.getKey());
@@ -40,4 +46,5 @@ public class S3CopyUtility {
         // Dummy logging
         System.out.println("Finished");
     }
+
 }
